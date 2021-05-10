e65b0d4052affa2dab3d620d3c3f4891ab4d3952
Sun XiaoWei <darell1986@gmail.com>
2021-05-10 15:34:16 +0800

    Fetch data use node request module

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const puppeteer = require('puppeteer')
 const express = require('express')
 const app = express()
+const request = require('request')
 const port = process.env.PORT || 3000
+var innerText = '';
 
-async function run(category, keyword) {
+function run(category, keyword) {
 
-    const browser = await puppeteer.launch( {
-        headless: true  //change to true in prod!
-    });
 
-    const page = await browser.newPage();
     const url = 'https://api.ali2woo.com/v1/get_products.php?version=1.15.2&page=1&per_page=20&keywords=' + keyword + '&category='.category;
 
-    await page.goto(url);
-
-    var content = await page.content();
-
-    innerText = await page.evaluate(() =>  {
-        return JSON.parse(document.querySelector("body").innerText);
-    });
-
-    //console.log("innerText now contains the JSON");
-    //console.log(innerText);
-
-    //I will leave this as an excercise for you to
-    //  write out to FS...
-
-    await browser.close();
+	request(url, function (error, response, body) {
+    	if (!error && response.statusCode === 200) {
+        	innerText = JSON.parse(body);
+     	}
+	});
 
     return innerText;
 
 };
 
 app.get('/api/products/:category/:keyword', (req, res) => {
-    run(req.params.category, req.params.keyword).then(result => {
-        res.send(result);
-    });
+    result = run(req.params.category, req.params.keyword);
+	res.send(result);
+
 });
 
 app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
